605c8dde018caf252b64acfd1778041bc91f24e5
XGZ <xgz.com>
2024-01-09 18:52:46 +0800

    星空代理微调，异地应该会需要短信验证码，还没解决

--- qd_xkdaili.py
+++ qd_xkdaili.py
@@ -1,10 +1,11 @@
 """
+星空代理微调
+2024-01-09 19:00
 const $ = new Env("星空代理签到");
 cron: 10 00 * * *
 """
 # 变量 export xingkong="账户1:密码&账户2:密码"
 import os
-import re
 from datetime import datetime
 
 import requests
@@ -14,23 +15,26 @@ from sendNotify import send
 try:
     xingkong = os.environ["xingkong"]
     headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # Requests sorts cookies= alphabetically
-        # 'Cookie': 'ASP.NET_SessionId=23dfn2mafqhzkuuzosirclt1; Hm_lvt_d76458121a7604d3e55d998f66ef0be6=1659492634; dt_cookie_user_name_remember=DTcms=18729469208; Hm_lpvt_d76458121a7604d3e55d998f66ef0be6=1659493214',
-        'DNT': '1',
-        'Origin': 'http://www.xkdaili.com',
-        'Referer': 'http://www.xkdaili.com/main/usercenter.aspx',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77',
-        'X-Requested-With': 'XMLHttpRequest',
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cache-Control": "no-cache",
+        "Content-Length": "10",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://www.xkdaili.com",
+        "Pragma": "no-cache",
+        "Referer": "https://www.xkdaili.com/main/UserCenter.aspx",
+        "Sec-Ch-Ua": "\"Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
+        "X-Requested-With": "XMLHttpRequest"
     }
     # 用于拼接
     msg = ""
-    params = {
-        'action': 'user_receive_point',
-    }
     # 按照空格分隔多个账户
     accounts = xingkong.split("&")
     for i in accounts:
@@ -38,25 +42,21 @@ try:
         data = {
             "username": up[0],
             "password": up[1],
-            "remember": 0
+            "remember": 1,
+            "code": ""
         }
         try:
-            aa = requests.post("http://www.xkdaili.com/tools/submit_ajax.ashx?action=user_login&site_id=1", headers=headers,
-                               data=data)
-            ck = aa.cookies
-            asp = re.findall(r"ASP\.NET_SessionId=(\w+)", str(ck))
-            dt = re.findall(r"dt_cookie_user_name_remember=(\w+=\w+)", str(ck))
-            cookies = {
-                "ASP.NET_SessionId": asp[0],
-                "dt_cookie_user_name_remember": dt[0]
-            }
+            sess = requests.session()
+            aa = sess.post("https://www.xkdaili.com/tools/submit_ajax.ashx?action=user_login&site_id=1",
+                           headers=headers,
+                           data=data)
 
             data = {
                 'type': 'login',
             }
 
-            response = requests.post('http://www.xkdaili.com/tools/submit_ajax.ashx', params=params, cookies=cookies,
-                                     headers=headers, data=data, verify=False)
+            response = sess.post('https://www.xkdaili.com/tools/submit_ajax.ashx?action=user_receive_point',
+                                 headers=headers, data=data, verify=False)
             txt = response.json()
             print("星空签到 ", txt['msg'])
             msg += f"账户 {up[0]} 星空签到 {txt['msg']}\n"
